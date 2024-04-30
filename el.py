bbb5f4874a9ba6c45d33936293c1712743a77d7d
olezuzu <72503544+olezuzu@users.noreply.github.com>
2024-04-30 18:36:50 +0200

    Add files via upload

--- el.py
+++ el.py
@@ -11,6 +11,7 @@ numbers = "1234567890"
 sorted_list = []
 
 for line in file:
+    line = line.replace("0","")
     line = line.replace("1","")
     line = line.replace("2","")
     line = line.replace("3","")
@@ -20,7 +21,6 @@ for line in file:
     line = line.replace("7","")
     line = line.replace("8","")
     line = line.replace("9","")
-    line = line.replace("0","")
     line = line.replace("\t","")
     output.write(line)
         
